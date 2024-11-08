06ea92f46ace331353c4b6d0bf0c5b95f5866700
Aliaksandr Artsikhovich <art-orient@tut.by>
2024-11-08 11:44:13 +0300

    studying some features

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 27
         targetSdk = 35
         versionCode = 1
-        versionName = "0.00"
+        versionName = "0.01"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
--- app/src/main/java/com/art/spy_camera/MainActivity.kt
+++ app/src/main/java/com/art/spy_camera/MainActivity.kt
@@ -1,20 +1,33 @@
 package com.art.spy_camera
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appNameTextView: TextView
+    private lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        appNameTextView = findViewById(R.id.textView)
+        val imageButton = findViewById<ImageButton>(R.id.imageButton)
+        editText = findViewById(R.id.editText)
+
+        imageButton.setOnClickListener {
+            if (editText.text.isEmpty()) {
+                appNameTextView.text = "Spy Camera"
+            } else {
+                appNameTextView.text = "Привет, " + editText.text
+            }
+            appNameTextView.text = "Фото сохранено"
         }
+
+
     }
 }
